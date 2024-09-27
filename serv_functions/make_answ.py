3fcde06ea905f7ddfcda851a42152923c4b54651
PROlodki <72821917+PROlodki@users.noreply.github.com>
2024-09-27 21:42:44 +0300

    Hotfix

--- serv_functions/make_answ.py
+++ serv_functions/make_answ.py
@@ -4,7 +4,7 @@ from Data.Answer import answer
 
 def create_answer(answer_text, id_question, is_right=False):
     db_sess = DB_session.create_session()
-    new_answer = answer(answer_text=answer_text,
+    new_answer = answer(answer=answer_text,
                         id_question=id_question,
                         is_right=is_right)
     db_sess.add(new_answer)
